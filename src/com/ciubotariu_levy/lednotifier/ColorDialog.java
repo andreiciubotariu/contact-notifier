39748073f0ac9dc93f57583acc41f5451b0a3dba
LevyMatthew <matthewlevy24@gmail.com>
2013-12-13 19:34:11 -0500

    Added ColorView

--- src/com/ciubotariu_levy/lednotifier/ColorDialog.java
+++ src/com/ciubotariu_levy/lednotifier/ColorDialog.java
@@ -5,14 +5,33 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 public class ColorDialog extends DialogFragment  {
+	ColorView colorView;
+	ColorView colorView2;
+	ColorView colorView3;
+	ColorView colorView4;
+
 	public ColorDialog(){
 		//Required Empty Constructor
 	}
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
-		View view = inflater.inflate(R.layout.color_dialog, container);		
+		getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+		View view = inflater.inflate(R.layout.color_dialog, container);
+		colorView = (ColorView) view.findViewById(R.id.color_view);
+		colorView.setColor(0xFF00FF00);
+		colorView.lockWidth=true;
+		colorView2 = (ColorView) view.findViewById(R.id.color_view2);
+		colorView2.setColor(0xFFFF0000);
+		colorView2.lockWidth=true;
+		colorView3 = (ColorView) view.findViewById(R.id.color_view3);
+		colorView3.setColor(0xFF0000FF);
+		colorView3.lockWidth=true;
+		colorView4 = (ColorView) view.findViewById(R.id.color_view4);
+		colorView4.setColor(0xFFFFFF00);
+		colorView4.lockWidth=true;
 		return view;
 		
 	}
---
+++ src/com/ciubotariu_levy/lednotifier/ColorView.java
@@ -0,0 +1,45 @@
+package com.ciubotariu_levy.lednotifier;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+//Graphical circle of color
+public class ColorView extends View {
+	int color;
+	Paint paint;
+	boolean lockWidth; //True if height depends on width, false if width depends on height
+
+	public ColorView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		paint = new Paint();
+		color = Color.BLACK;
+		paint.setColor(color);
+	}
+
+	public void setColor(int color){
+		this.color = color;
+		paint.setColor(color);
+		invalidate();
+	}
+
+	public int getColor(){
+		return color;
+	}
+
+	public void onMeasure(int w, int h){
+		if (lockWidth)
+			super.onMeasure(w, w);
+		else
+			super.onMeasure(h, h);
+		invalidate();
+	}
+
+	public void onDraw(Canvas c){
+		c.drawCircle(getWidth()/2, getHeight()/2, getWidth()/2, paint);
+	}
+
+}
