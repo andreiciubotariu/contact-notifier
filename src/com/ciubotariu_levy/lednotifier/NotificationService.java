5f8a8ae72389d004802ef2d27884878acc498908
Andrei <andrei.development@gmail.com>
2013-12-19 20:01:35 -0500

    Prepping for replace option

--- src/com/ciubotariu_levy/lednotifier/NotificationService.java
+++ src/com/ciubotariu_levy/lednotifier/NotificationService.java
@@ -6,20 +6,41 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.provider.Telephony;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class NotificationService extends NotificationListenerService {
 	private final static String TAG = "NotificationService";
+	private final static String KEY_REPLACE_NOTIFICATION = "replace_notification";
+
 	protected static boolean isNotificationListenerServiceOn = false;
 	private NotificationManager mNotifyManager; 
 	private Notification mCurrentNotification = null;
+	private boolean mReplaceNotification = false;
+
+	private SharedPreferences.OnSharedPreferenceChangeListener prefListener = new OnSharedPreferenceChangeListener (){
+
+		@Override
+		public void onSharedPreferenceChanged(
+				SharedPreferences sharedPreferences, String key) {
+			if (KEY_REPLACE_NOTIFICATION.equals(key)){
+				mReplaceNotification = sharedPreferences.getBoolean(key, false);
+			}
+		}
+	};
+
+
+
 	private SMSReceiver mSmsReceiver = new SMSReceiver(){
 
 		@Override
@@ -30,7 +51,20 @@ public class NotificationService extends NotificationListenerService {
 				Log.d(TAG, "returning");
 				return;
 			}
-			mCurrentNotification = notif;
+			if (mReplaceNotification){
+				StatusBarNotification [] notifications = getActiveNotifications();
+				for (StatusBarNotification sb: notifications){
+					if (isMessagingApp(sb.getPackageName()) && sb.isClearable()){
+						int color = notif.ledARGB;
+						notif = copyNotification(context, sb.getNotification(),color);
+						cancelNotification(sb.getPackageName(), sb.getTag(), sb.getId());;
+						break;
+					}
+				}
+			}
+			else {
+				mCurrentNotification = notif;
+			}
 			notify (context, notif);
 		}
 	};
@@ -39,15 +73,17 @@ public class NotificationService extends NotificationListenerService {
 	@Override
 	public void onCreate(){
 		super.onCreate();
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		sharedPrefs.registerOnSharedPreferenceChangeListener(prefListener);
+		mReplaceNotification = sharedPrefs.getBoolean(KEY_REPLACE_NOTIFICATION, false);
 		String filterAction = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ?
 				Telephony.Sms.Intents.SMS_RECEIVED_ACTION :
-				"android.provider.Telephony.SMS_RECEIVED";
+					"android.provider.Telephony.SMS_RECEIVED";
 		mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		IntentFilter filter = new IntentFilter (filterAction);
 		registerReceiver(mSmsReceiver, filter);
-		
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent){
 		isNotificationListenerServiceOn = true;
@@ -56,6 +92,7 @@ public class NotificationService extends NotificationListenerService {
 
 	@Override
 	public void onDestroy (){
+		PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(prefListener);
 		unregisterReceiver(mSmsReceiver);
 		isNotificationListenerServiceOn = false;
 		super.onDestroy();
@@ -67,6 +104,12 @@ public class NotificationService extends NotificationListenerService {
 				"\n"+sbn.getNotification().tickerText +
 				"\n"+sbn.getPackageName());
 		if (mCurrentNotification != null && isMessagingApp(sbn.getPackageName())){
+			if (mReplaceNotification){
+				int color = mCurrentNotification.ledARGB;
+				mCurrentNotification = copyNotification(this, sbn.getNotification(),color);
+				cancelNotification(sbn.getPackageName(), sbn.getTag(), sbn.getId());
+			}
+
 			SMSReceiver.notify (this, mCurrentNotification);
 		}
 	}
@@ -74,12 +117,15 @@ public class NotificationService extends NotificationListenerService {
 
 	@Override
 	public void onNotificationRemoved(StatusBarNotification sbn) {
-		if (isMessagingApp(sbn.getPackageName())){
+		if (mReplaceNotification && sbn.getPackageName().equals(getPackageName())){
+			mCurrentNotification = null;
+		}
+		else if (!mReplaceNotification && isMessagingApp(sbn.getPackageName())){
 			((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).cancel(SMSReceiver.NOTIFICATION_ID);
 			mCurrentNotification = null;
 		}
 	}
-	
+
 	@TargetApi(Build.VERSION_CODES.KITKAT)
 	private boolean isMessagingApp (String packageName){
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
@@ -87,5 +133,17 @@ public class NotificationService extends NotificationListenerService {
 		}
 		return (!packageName.equals(getPackageName())) && (packageName.contains("mms")||packageName.contains("sms") || packageName.contains("messaging")
 				||packageName.contains("message"));
+	}
+
+	private static Notification copyNotification (Context context, Notification toCopy, int color){
+		return new NotificationCompat.Builder(context)
+		.setSmallIcon(R.drawable.ic_launcher)
+		.setTicker(toCopy.tickerText, toCopy.tickerView)
+		.setContent(toCopy.contentView)
+		.setAutoCancel(true)
+		.setContentIntent(toCopy.contentIntent)
+		//.setSound(toCopy.sound)
+		.setLights(color, 1000, 1000)
+		.build();
 	}
 }
